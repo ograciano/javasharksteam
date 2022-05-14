4663b81198cc735fe21febbc465e3b22b2193ecb
ograciano <cev3067@innovaccion.mx>
2022-05-14 00:21:28 -0500

    Confguracion basica del servidor express

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
+const Server = require('./app/models/server');
+
 require('dotenv').config();
+
+const server = new Server();
+
+server.listen();
---
+++ app/models/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require('cors');
+
+class Server {
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT || 8080;
+        this.path = { denuncia: '/api/v1/denuncias'};
+        this.routes()
+        this.middlewares()
+    }
+
+    routes(){
+        // this.app.use(this.path.denuncia, require('../routes/denuncia'))
+    }
+
+    middlewares(){
+        this.app.use(cors());
+        this.app.use(express.json());
+        this.app.use(express.static('public'));
+    }
+
+    listen() {
+        this.app.listen(this.port, () => console.log(`Servidor corriendo en el puerto ${this.port}`));
+    }
+
+
+}
+
+module.exports = Server;
