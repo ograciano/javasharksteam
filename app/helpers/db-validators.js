0551737a21f1303fd07079d72aaee90b2ef3f930
ograciano <cev3067@innovaccion.mx>
2022-05-15 11:30:35 -0500

    WW

---
+++ app/helpers/db-validators.js
@@ -0,0 +1,45 @@
+const { Categoria, Producto, Usuario, Role } = require("../models");
+
+
+const esRoleValido = async (rol = '') => {
+    const existeRole = await Role.findOne({ role: rol });
+    if (!existeRole) {
+        throw new Error(`El role ${rol} no esta registrado en la base de datos`);
+    }
+}
+
+const existEmail = async (correo = '') => {
+    const existeEmail = await Usuario.findOne({ correo });
+    if (existeEmail) {
+        throw new Error(`este correo: ${correo} ya esta registrado`)
+    }
+}
+
+const existUsuario = async (id) => {
+    const existeUsuario = await Usuario.findById(id);
+    if (!existeUsuario) {
+        throw new Error(`El ID no existe: ${id}`)
+    }
+}
+
+const existCategoria = async (id) => {
+    const existeCategoria = await Categoria.findById(id);
+    if (!existeCategoria) {
+        throw new Error(`El ID no existe: ${id}`)
+    }
+}
+
+const existProducto = async (id) => {
+    const existeProducto = await Producto.findById(id);
+    if (!existeProducto) {
+        throw new Error(`El ID no existe: ${id}`)
+    }
+}
+
+module.exports = {
+    esRoleValido,
+    existEmail,
+    existUsuario,
+    existCategoria,
+    existProducto
+}
---
+++ app/helpers/subir-archivo.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
+
+const subirArchivo = (files, extensionesValidas = ['png', 'jpg', 'jpeg', 'gif', 'pdf'], carpeta = '') => {
+    return new Promise((resolve, reject) => {
+        const { archivo } = files;
+
+        const nombreCortado = archivo.name.split('.');
+        const extension = nombreCortado[nombreCortado.length - 1];
+
+        if (!extensionesValidas.includes(extension)) {
+            return reject(`la estencion ${extension} no es permitida, ${extensionesValidas}` )
+        }
+
+        const nombreTemp = uuidv4() + '.' + extension;
+        const uploadPath = path.join(__dirname, '../uploads', carpeta, nombreTemp);
+
+        archivo.mv(uploadPath, (err) => {
+            if (err) {
+                reject(err)
+            }
+            resolve(uploadPath)
+        });
+    });
+}
+
+module.exports = subirArchivo;
