9fe279cd5d991340999a2883325dbc8901e2567a
ograciano <cev3067@innovaccion.mx>
2022-05-15 14:50:44 -0500

    Update a conecx

--- app/database/connection.js
+++ app/database/connection.js
@@ -5,9 +5,10 @@ const db_name = process.env.DB_NAME;
 const db_password = process.env.PASSWORD_SQL;
 const db_user = process.env.DB_USER;
 const db_port = process.env.DB_PORT;
+const db_host = process.env.DB_HOST;
 const db = new Sequelize(db_name, db_user, db_password, {
     dialect: 'mssql',
-    host: 'localhost',
+    host: db_host,
     port: db_port
 })
 
